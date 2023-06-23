554794de42dcd603c3e27a7407ebb60e95f09e82
ydv-sahitya <11sahitya@gmail.com>
2023-06-23 14:19:15 +0530

    added prettierrc and eslingrc

--- src/app.js
+++ src/app.js
@@ -1,54 +1,40 @@
-
 // this example using props. more flexible way
 
-
 const Pet = (props) => {
-    return React.createElement("div",{}, [
-        React.createElement("h1",{}, props.name),
-        React.createElement("h2",{}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]); 
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
 
-
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1",{}, "Adopt Me!"),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Luna",
-                breed: "havanese"
-            }),
-            React.createElement(Pet, {
-                animal: "Another dog",
-                name: "july",
-                breed: "desi breed"
-            }),
-            React.createElement(Pet, {
-                animal: "cat",
-                name: "meaow",
-                breed: "desi cat"
-            }),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Luna",
+      breed: "havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Another dog",
+      name: "july",
+      breed: "desi breed",
+    }),
+    React.createElement(Pet, {
+      animal: "cat",
+      name: "meaow",
 
-        ]
-        
-    )
+      breed: "desi cat",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
 
-
-
-
 // -------------------------------------------------------------------------------------
-
-
-
 
 // this is a first verions. not very flexible.
 
@@ -57,9 +43,8 @@ root.render(React.createElement(App));
 //         React.createElement("h1",{},"Luna"),
 //         React.createElement("h2",{}, "Dog"),
 //         React.createElement("h2", {}, "Havanese"),
-//     ]); 
+//     ]);
 // };
-
 
 // const App = () => {
 //     return React.createElement(
@@ -72,7 +57,7 @@ root.render(React.createElement(App));
 //             React.createElement(Pet),
 
 //         ]
-        
+
 //     )
 // };
 
